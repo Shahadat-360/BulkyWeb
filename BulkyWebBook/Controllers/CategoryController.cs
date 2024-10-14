9d0cadfaec7b8f1aaa9a4c22afa2f0a689e7d510
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-10-14 12:25:00 +0600

    Validation using built in validation(Server SIde Validation)

---
+++ BulkyWebBook/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using BulkyWebBook.Data;
+using BulkyWebBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWebBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly BulkyWebDbContext _db;
+        public CategoryController(BulkyWebDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+    }
+}
--- BulkyWebBook/Program.cs
+++ BulkyWebBook/Program.cs
@@ -1,7 +1,13 @@
+using BulkyWebBook.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BulkyWebDbContext>(options =>
+                options.UseSqlServer(builder.Configuration
+                .GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
